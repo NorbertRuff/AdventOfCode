79b5d6396eeecdb9398b918802a9715456f10283
Norbert Ruff <norbi@apexlab.io>
2022-12-02 09:41:49 +0100

    [feat]: second day not that ugly

--- day2/day2_resolver.py
+++ day2/day2_resolver.py
@@ -2,80 +2,75 @@ from utils import read_file
 from enum import Enum
 
 
-# Enum class is now a thing. Syntax https://docs.python.org/3/library/enum.html
-class RockPaperScissorsPlayer(Enum):
-    A = "ROCK"
-    B = "PAPER"
-    C = "SCISSORS"
+# Enum class is now a thing in python. Syntax https://docs.python.org/3/library/enum.html
+class RockPaperScissors(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
 
 
-class RockPaperScissorsEnemy(Enum):
-    X = "ROCK"
-    Y = "PAPER"
-    Z = "SCISSORS"
+# So I used it
+class Symbols(Enum):
+    A = X = RockPaperScissors.ROCK
+    B = Y = RockPaperScissors.PAPER
+    C = Z = RockPaperScissors.SCISSORS
 
 
-def play_a_game(player, enemy):
-    if player == RockPaperScissorsPlayer.A:
-        if enemy == RockPaperScissorsEnemy.X:
-            return 4
-        if enemy == RockPaperScissorsEnemy.Y:
-            return 8
-        elif enemy == RockPaperScissorsEnemy.Z:
-            return 3
-    elif player == RockPaperScissorsPlayer.B:
-        if enemy == RockPaperScissorsEnemy.X:
-            return 1
-        if enemy == RockPaperScissorsEnemy.Y:
-            return 5
-        elif enemy == RockPaperScissorsEnemy.Z:
-            return 9
-    elif player == RockPaperScissorsPlayer.C:
-        if enemy == RockPaperScissorsEnemy.X:
-            return 7
-        if enemy == RockPaperScissorsEnemy.Y:
-            return 2
-        elif enemy == RockPaperScissorsEnemy.Z:
-            return 6
+# a lot
+class Score(Enum):
+    WIN = 6
+    LOSE = 0
+    DRAW = 3
 
 
-def get_day2_results_for_first_part():
+def play_a_game_part_1(player: RockPaperScissors, enemy: RockPaperScissors) -> int:
+    if player == RockPaperScissors.ROCK:
+        if enemy == RockPaperScissors.ROCK:
+            return Score.DRAW.value
+        elif enemy == RockPaperScissors.PAPER:
+            return Score.LOSE.value
+        elif enemy == RockPaperScissors.SCISSORS:
+            return Score.WIN.value
+    elif player == RockPaperScissors.PAPER:
+        if enemy == RockPaperScissors.PAPER:
+            return Score.DRAW.value
+        elif enemy == RockPaperScissors.ROCK:
+            return Score.WIN.value
+        elif enemy == RockPaperScissors.SCISSORS:
+            return Score.LOSE.value
+    elif player == RockPaperScissors.SCISSORS:
+        if enemy == RockPaperScissors.SCISSORS:
+            return Score.DRAW.value
+        elif enemy == RockPaperScissors.ROCK:
+            return Score.LOSE.value
+        elif enemy == RockPaperScissors.PAPER:
+            return Score.WIN.value
+
+
+def play_a_game_part_2(enemy: RockPaperScissors):
+    if enemy == RockPaperScissors.ROCK:
+        return Score.DRAW.value
+    elif enemy == RockPaperScissors.PAPER:
+        return Score.LOSE.value
+    elif enemy == RockPaperScissors.SCISSORS:
+        return Score.WIN.value
+
+
+def get_day2_results_for_first_part() -> int:
     raw_data = read_file("./day2/day2-input.txt").splitlines()
     result = 0
     for line in raw_data:
-        x, y = line.split(" ") # tuple unpacking
-        result += play_a_game(RockPaperScissorsPlayer[x], RockPaperScissorsEnemy[y])
-    print(result)
+        x, y = line.split(" ")  # tuple unpacking
+        result += Symbols[y].value.value
+        result += play_a_game_part_1(Symbols[y].value, Symbols[x].value)
+    return result
 
 
-def play_a_game_part2(player, enemy):
-    if player == RockPaperScissorsPlayer.A:
-        if enemy == RockPaperScissorsEnemy.X:
-            return 3
-        if enemy == RockPaperScissorsEnemy.Y:
-            return 4
-        elif enemy == RockPaperScissorsEnemy.Z:
-            return 8
-    elif player == RockPaperScissorsPlayer.B:
-        if enemy == RockPaperScissorsEnemy.X:
-            return 1
-        if enemy == RockPaperScissorsEnemy.Y:
-            return 5
-        elif enemy == RockPaperScissorsEnemy.Z:
-            return 9
-    elif player == RockPaperScissorsPlayer.C:
-        if enemy == RockPaperScissorsEnemy.X:
-            return 2
-        if enemy == RockPaperScissorsEnemy.Y:
-            return 6
-        elif enemy == RockPaperScissorsEnemy.Z:
-            return 7
-
-
-def get_day2_results_for_second_part():
+def get_day2_results_for_second_part() -> int:
     raw_data = read_file("./day2/day2-input.txt").splitlines()
     result = 0
     for line in raw_data:
-        x, y = line.split(" ") # tuple unpacking
-        result += play_a_game_part2(RockPaperScissorsPlayer[x], RockPaperScissorsEnemy[y])
-    print(result)
+        x, y = line.split(" ")  # tuple unpacking
+        result += Symbols[y].value.value
+        result += play_a_game_part_2(Symbols[x].value)
+    return result
