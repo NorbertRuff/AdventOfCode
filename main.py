6d59a2ba89e2f806453920c52a040f9cecab8fde
Norbert Ruff <norbi@apexlab.io>
2022-12-03 07:24:29 +0100

    [feat]: day3

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from day3 import get_day3_results_for_first_part, get_day3_results_for_second_pa
 
 def main():
     elf_calories = get_results_for_first_part()
-    # print("day 1 first part result", max(elf_calories))
-    # sum_of_calories_for_first_3_elf = get_results_for_second_part(elf_calories)
-    # print("day 1 second part result", sum_of_calories_for_first_3_elf)
-    # print("day 2 first part result", get_day2_results_for_first_part())
-    # print("day 2 second part result", get_day2_results_for_second_part())
-    # print("day 3 first part result", get_day3_results_for_first_part())
+    print("day 1 first part result", max(elf_calories))
+    sum_of_calories_for_first_3_elf = get_results_for_second_part(elf_calories)
+    print("day 1 second part result", sum_of_calories_for_first_3_elf)
+    print("day 2 first part result", get_day2_results_for_first_part())
+    print("day 2 second part result", get_day2_results_for_second_part())
+    print("day 3 first part result", get_day3_results_for_first_part())
     print("day 3 second part result", get_day3_results_for_second_part())
 
 
