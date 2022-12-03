efbca0e207817a7cceba2526dcf612925e8fc5bb
Norbert Ruff <norbi@apexlab.io>
2022-12-03 07:23:11 +0100

    [feat]: day3

--- day3/day3_resolver.py
+++ day3/day3_resolver.py
@@ -1,11 +1,48 @@
 from utils import read_file
 
 
+LOWERCASE_SHIFT = 96
+UPPERCASE_SHIFT = 38
+
+
 def get_day3_results_for_first_part():
     raw_data = read_file("./day3/day3-input.txt").splitlines()
-    return 0
+    low_priority = []
+    high_priority = []
+    for line in raw_data:
+        rucksack1_items = set(line[:len(line) // 2])
+        rucksack2_items = set(line[len(line) // 2:])
+        print(rucksack1_items, rucksack2_items)
+        for item in rucksack1_items:
+            if item in rucksack2_items:
+                if item.islower():
+                    low_priority.append(item)
+                elif item.isupper():
+                    high_priority.append(item)
+
+    print(low_priority, high_priority)
+    print(ord('A')-UPPERCASE_SHIFT, ord('z')-LOWERCASE_SHIFT)
+    ascii_low_priority = [ord(item)-LOWERCASE_SHIFT for item in low_priority]
+    ascii_high_priority = [ord(item)-UPPERCASE_SHIFT for item in high_priority]
+    return sum(ascii_low_priority)+sum(ascii_high_priority)
 
 
 def get_day3_results_for_second_part():
     raw_data = read_file("./day3/day3-input.txt").splitlines()
-    return 0
+    result = 0
+    try:
+        for i in range(0, len(raw_data), 3):
+            first_rucksack = set(raw_data[i])
+            second_rucksack = set(raw_data[i+1])
+            third_rucksack = set(raw_data[i+2])
+            common_chars = third_rucksack.intersection(first_rucksack.intersection(second_rucksack))
+            print(common_chars)
+            for item in common_chars:
+                if item.islower():
+                    result += ord(item) - LOWERCASE_SHIFT
+                elif item.isupper():
+                    result += ord(item) - UPPERCASE_SHIFT
+    except IndexError:
+        pass
+    return result
+
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from day1 import get_results_for_first_part, get_results_for_second_part
 from day2 import get_day2_results_for_first_part, get_day2_results_for_second_part
+from day3 import get_day3_results_for_first_part, get_day3_results_for_second_part
 
 
 def main():
     elf_calories = get_results_for_first_part()
-    print("day 1 first part result", max(elf_calories))
-    sum_of_calories_for_first_3_elf = get_results_for_second_part(elf_calories)
-    print("day 1 second part result", sum_of_calories_for_first_3_elf)
-    print("day 2 first part result", get_day2_results_for_first_part())
-    print("day 2 second part result", get_day2_results_for_second_part())
+    # print("day 1 first part result", max(elf_calories))
+    # sum_of_calories_for_first_3_elf = get_results_for_second_part(elf_calories)
+    # print("day 1 second part result", sum_of_calories_for_first_3_elf)
+    # print("day 2 first part result", get_day2_results_for_first_part())
+    # print("day 2 second part result", get_day2_results_for_second_part())
+    # print("day 3 first part result", get_day3_results_for_first_part())
+    print("day 3 second part result", get_day3_results_for_second_part())
+
 
 
 if __name__ == '__main__':
