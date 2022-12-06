2b24c796fd746d8676cd1cd536a40ea443cb9a8f
Norbert Ruff <norbi@apexlab.io>
2022-12-06 07:13:12 +0100

    [feat]: day6

---
+++ day6/day6_resolver.py
@@ -0,0 +1,25 @@
+from utils import read_file
+
+
+def get_day6_results_for_first_part():
+    # input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    # input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    # input = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    # input = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    raw_data = read_file("./day6/day6-input.txt")
+    for i in range(len(raw_data)):
+        characters = 4
+        batch = raw_data[(i - characters):i]
+        print(batch)
+        if len(set(batch)) == characters:
+            return i
+
+
+def get_day6_results_for_second_part():
+    raw_data = read_file("./day6/day6-input.txt")
+    for i in range(len(raw_data)):
+        characters = 14
+        batch = raw_data[(i - characters):i]
+        print(batch)
+        if len(set(batch)) == characters:
+            return i
