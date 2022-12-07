9596cb0185f0ff9d4106f6bf8e300dbbde48085e
Norbert Ruff <norbi@apexlab.io>
2022-12-07 18:38:56 +0100

    [feat]: day7

--- day7/day7_resolver.py
+++ day7/day7_resolver.py
@@ -1,4 +1,4 @@
-from utils import read_file, read_file_no_strip
+from utils import read_file_no_strip
 
 raw_data = read_file_no_strip("./day7/day7-input.txt")
 
@@ -7,10 +7,9 @@ def get_day7_results():
     current_directory = []
     directories = {}
     for line in raw_data.splitlines():
-        total = 0
         x, command, *args = line.strip().split()
         args = args[0] if args else None
-        if x != "$" and x != "dir":
+        if x.isdigit():
             directory = '/'.join(current_directory)
             while directory:
                 directories[directory][0] += int(x)
@@ -23,7 +22,7 @@ def get_day7_results():
             directory = '/'.join(current_directory)
             if directory not in directories:
                 directories[directory] = [0, '/'.join(current_directory[:-1])]
-    total = sum(size for size, _ in directories.values() if size <= 100000)
+    part_a = sum(size for size, _ in directories.values() if size <= 100000)
     path = [size for size, _ in directories.values()]
     part_b = [size for size in sorted(path) if size >= 30000000 - (70000000 - directories["/"][0])][0]
-    return total, part_b
+    return part_a, part_b
